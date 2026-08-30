21f6b1c4b56b2f84f8da91c4e189db0352602a61
alimkulov7 <alimkulovakbar7@gmail.com>
2026-08-30 19:42:49 +0900

    feat: completed training Callback and Async functions

--- train.js
+++ train.js
@@ -1,1 +1,109 @@
-console.log("Train area");
+//CALL-BACK FUNCIONS 21
+
+console.log("Jack Ma maslahatlari");
+
+const list = [
+  "yaxshi talaba bo'ling", // 0–20
+  "tog'ri boshliq tanlang va koproq hato qiling", // 20–30
+  "uzingizga ishlandingiz boshing", // 30–40
+  "siz kuchli bolgan narsalarni qiling", // 40–50
+  "yoshlarga investitsiya qiling", // 50–60
+  "endi dam oling, foydasi yoq endi" // 60
+];
+
+/* function maslahatBering (a, callback) {
+    setTimeout(function () {
+        if (typeof a !== "number" ) callback("insert a number",null);
+    else if (a<=20) callback(null, list[0]);
+    else if (a>20 && a<=30) callback(null,list[1]);
+    else if (a>30 && a<=40) callback(null,list[2]);
+    else if (a>40 && a<=50) callback(null,list[3]);
+    else if (a>50 && a<=60) callback(null,list[4]);
+    else  {
+        callback(null,list[5]);
+    }
+    },1000);
+}
+console.log('Passed here 1');
+maslahatBering (22, (err, data) => {
+    if (err) console.log("ERROR: ",err);
+    else{
+        console.log('JAVOB: ',data);
+    }
+});
+console.log('Passed here 2'); */
+
+//ASYNCHRONOUS FUNCTIONS 22
+
+/* async function maslahatBering (a) {
+    
+        if (typeof a !== "number" ) callback("insert a number",null);
+    else if (a<=20) return  list[0];
+    else if (a>20 && a<=30) return  list[1];
+    else if (a>30 && a<=40) return  list[2];
+    else if (a>40 && a<=50) return  list[3];
+    else if (a>50 && a<=60) return  list[4];
+    else  {
+        return new Promise ((resolve, reject) => {
+            setTimeout(() => {
+                resolve(list[5]);
+            },3000);
+        });
+    }
+    
+}
+ */
+//then catch 
+
+/* console.log('Passed here 1');
+
+maslahatBering (22)
+    .then((data) => {
+    console.log('JAVOB: ', data );
+    })
+    .catch((err) => {
+    console.log("ERROR: ",err);
+    }) ;
+    
+console.log('Passed here 2'); */
+
+// Calling function with ASYNC / AWAIT 
+
+/* async function run() {
+    let res = await maslahatBering(22);
+    console.log(res);
+    res = await maslahatBering(71);
+    console.log(res);
+    res = await maslahatBering(41);
+    console.log(res);
+}
+
+run();
+ */
+
+//CALLBACK & SET INTERVAL
+
+function maslahatBering (a, callback) {
+    
+        if (typeof a !== "number" ) callback("insert a number",null);
+    else if (a<=20) callback(null, list[0]);
+    else if (a>20 && a<=30) callback(null,list[1]);
+    else if (a>30 && a<=40) callback(null,list[2]);
+    else if (a>40 && a<=50) callback(null,list[3]);
+    else if (a>50 && a<=60) callback(null,list[4]);
+    else  {
+        setInterval( function () {
+            callback(null,list[5]);
+        },1000);
+    }
+}
+    
+
+console.log("Passed here 1 ");
+maslahatBering (74, (err, data) => {
+    if (err) console.log("ERROR: ",err);
+    else{
+        console.log('JAVOB: ',data);
+    }
+});
+console.log("Passed here 2");
